5acb73e7069bfcaa58a8e2c7d2db50ba3e637078
Kareem ElZeky <kareem.elzeky@gmail.com>
2023-09-29 19:28:21 +0300

    Build: Added router

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,36 @@
-import './App.css';
+import "./App.css";
 
+import Login from "./Features/Login.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return;
+  return (
+    <>
+      <Router>
+        <div className="app">
+          <main>
+            {/* <Products /> */}
+            <Routes>
+              <Route path="/login" exact element={<Login />} />
+              <Route
+                path="*"
+                element={
+                  <h1
+                    style={{
+                      paddingTop: 5 + "%",
+                      height: 75 + "vh",
+                      textAlign: "center",
+                    }}
+                  >
+                    404 Not Found!
+                  </h1>
+                }
+              />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
