d9099954b7a23dd8e71698ee1bca689e90608012
Kareem ElZeky <kareem.elzeky@gmail.com>
2023-09-29 19:08:26 +0300

    Build: Added user slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import userReducer from "../features/Login/UserSlics";
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
 
 export default store;
---
+++ src/features/Login/UserSlics.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const user = createSlice({
+  name: "user",
+  initialState: {
+    isAuthenticated: false,
+    data: {},
+  },
+  reducers: {
+    authUser: (state, { payload }) => {
+      state.isAuthenticated = true;
+      state.data = payload;
+    },
+    logout: (state, { payload }) => {
+      state.isAuthenticated = false;
+      state.data = {};
+    },
+  },
+});
+export const selectUser = (state) => {
+  return state.user.data;
+};
+export const selectIsAuthenticated = (state) => {
+  return state.user.isAuthenticated;
+};
+
+export const { authUser, logout } = user.actions;
+export default user.reducer;
